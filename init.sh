096f9aec56a8f15b6bd7fafbf76a219acd2415f1
bospina-lealco <bospina@leal.co>
2023-04-21 17:59:57 -0500

    change bash

--- init.sh
+++ init.sh
@@ -4,5 +4,6 @@
 kubectl create secret generic mysql-secrets --from-literal=openmetadata-mysql-password=openmetadata_password -n opendata
 kubectl create secret generic airflow-secrets --from-literal=openmetadata-airflow-password=admin -n opendata
 kubectl create secret generic airflow-mysql-secrets --from-literal=airflow-mysql-password=airflow_pass -n opendata
-#helm upgrade -i openmetadata open-metadata/openmetadata --values values.yaml -n opendata
 #helm upgrade -i openmetadata-dependencies open-metadata/openmetadata-dependencies --values values-dependencies.yaml -n opendata
+#helm upgrade -i openmetadata open-metadata/openmetadata --values values.yaml -n opendata
+
